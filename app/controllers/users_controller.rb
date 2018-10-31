b73d5c798526d6f6b41b00bcf685fe4dbdcea8d8
kanegon9365 <kanegon9365@gmail.com>
2018-10-31 21:14:22 +0900

    ログインとログアウト機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to succeed_path
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   root 'users#home'
   get '/about', to: 'users#about'
   get '/signup', to: 'users#new'
