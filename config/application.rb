637d65f99ab3be894575d34c36455219c78421ef
kanegon9365 <kanegon9365@gmail.com>
2018-11-26 18:21:18 +0900

    compile

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module TeddyApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.initialize_on_precompile = false
   end
 end
