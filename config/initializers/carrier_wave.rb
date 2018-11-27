3fd967e619b48b90dd1f92e56a437f844a72e3ab
kanegon9365 <kanegon9365@gmail.com>
2018-11-27 17:55:19 +0900

    a

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,20 +1,20 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  config.storage :fog
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: 'AKIAJ75WZPBHP7OPNSEA',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1'
-    }
+# if Rails.env.production?
+#   config.storage :fog
+#   CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       provider: 'AWS',
+#       aws_access_key_id: 'AKIAJ75WZPBHP7OPNSEA',
+#       aws_secret_access_key: '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',
+#       region: 'ap-northeast-1'
+#     }
   
-    config.fog_directory  = 'rails-photo-123'
-    config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/kanegon-teddy-app"
-    config.cache_storage = :fog
-  end
-end
+#     config.fog_directory  = 'rails-photo-123'
+#     config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/kanegon-teddy-app"
+#     config.cache_storage = :fog
+#   end
+# end
 
